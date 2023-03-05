ded43775dd63e08f8099f6e0392763ec9218b0d1
Adam Němeček <nemeceka22@spstrutnov.cz>
2023-03-05 18:40:57 +0100

    Pohyb hráče nahoru a dolu

--- No gold for goblins.py
+++ No gold for goblins.py
@@ -12,7 +12,7 @@ pozadi = pygame.image.load("pozadi.png")
 pozadi = pygame.transform.scale(pozadi, (ROZLISENI_X, ROZLISENI_Y))
 tank = pygame.image.load("tank.png")
 tank = pygame.transform.scale(tank, (velikostx, velikosty))
-
+rychlost = 5
 
 pozice_x = (ROZLISENI_X - velikostx) / 2
 pozice_y = (ROZLISENI_Y - velikosty) / 2
@@ -22,15 +22,24 @@ pozice_y = (ROZLISENI_Y - velikosty) / 2
 
 hodiny = pygame.time.Clock()
 okno = pygame.display.set_mode((ROZLISENI_X, ROZLISENI_Y))
-okno.blit(pozadi, (0, 0))
-okno.blit(tank, (pozice_x, pozice_y)) 
 pygame.display.set_caption("No gold for goblins")
 
 
+
+
 while True:
+    okno.blit(pozadi, (0, 0))
+    okno.blit(tank, (pozice_x, pozice_y))
+    klavesy = pygame.key.get_pressed()
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
+
+    if klavesy[pygame.K_DOWN]:
+        pozice_y += rychlost
+    if klavesy[pygame.K_UP]:
+        pozice_y -= rychlost
     pygame.display.update()
     hodiny.tick(FPS)
