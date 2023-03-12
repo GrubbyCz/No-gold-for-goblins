86ce75fe744f5ac539b1575ac56b82ca3082b19c
Adam Němeček <nemeceka22@spstrutnov.cz>
2023-03-12 12:42:23 +0100

    kontroly

--- No gold for goblins.py
+++ No gold for goblins.py
@@ -3,19 +3,19 @@ import pygame
 
 pygame.init()
 
-velikostx = 150
-velikosty = 200
+velikost_x = 150
+velikost_y = 200
 ROZLISENI_X = 1920
 ROZLISENI_Y = 1022
 FPS = 60
 pozadi = pygame.image.load("pozadi.png")
 pozadi = pygame.transform.scale(pozadi, (ROZLISENI_X, ROZLISENI_Y))
 tank = pygame.image.load("tank.png")
-tank = pygame.transform.scale(tank, (velikostx, velikosty))
+tank = pygame.transform.scale(tank, (velikost_x, velikost_y))
 rychlost = 5
 
-pozice_x = (ROZLISENI_X - velikostx) / 2
-pozice_y = (ROZLISENI_Y - velikosty) / 2
+pozice_x = (ROZLISENI_X - velikost_x) / 2
+pozice_y = (ROZLISENI_Y - velikost_y) / 2
 
 
 #zobrazení okna
@@ -41,5 +41,18 @@ while True:
         pozice_y += rychlost
     if klavesy[pygame.K_UP]:
         pozice_y -= rychlost
+        
+        
+        
+        
+    if pozice_x > ROZLISENI_X - velikost_x:
+        pozice_x = ROZLISENI_X - velikost_x
+    if pozice_y > ROZLISENI_Y - velikost_y:
+        pozice_y = ROZLISENI_Y - velikost_y
+    if pozice_x < 0:
+        pozice_x = 0
+    if pozice_y < 0:
+        pozice_y = 0
+        
     pygame.display.update()
     hodiny.tick(FPS)
